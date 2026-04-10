eb2163d9df310c9b10e9323f244b2db1665e0d55
Родион Карпов <r.karpov3332mail.com@mail.ru>
2026-04-10 17:51:42 +0300

    подключение к базе данных, вывод количества персон

--- gramps60/download/CompletenessGramplet/completeness_dashboard.py
+++ gramps60/download/CompletenessGramplet/completeness_dashboard.py
@@ -5,11 +5,22 @@ from gramps.gen.plug import Gramplet
 class CompletenessGramplet(Gramplet):
 
     def init(self):
-        self.set_text("Загрузка...")
+        self.set_text("Нет данных")
 
     def db_changed(self):
+        self.connect(self.dbstate.db, "person-add", self.update)
+        self.connect(self.dbstate.db, "person-update", self.update)
+        self.connect(self.dbstate.db, "person-delete", self.update)
         self.connect(self.dbstate.db, "person-rebuild", self.update)
 
     def main(self):
-        self.set_text("Gramplet работает!\n")
+        db = self.dbstate.db
+        total = db.get_number_of_people()
+
+        if total == 0:
+            self.set_text("База данных пуста\n")
+            yield False
+            return
+
+        self.set_text("Всего персон: %d\n" % total)
         yield False
