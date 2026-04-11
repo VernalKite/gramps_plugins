3dc8d3996b561d9fbba28057b8338b9df6347671
Родион Карпов <r.karpov3332mail.com@mail.ru>
2026-04-11 11:07:05 +0300

    подсчёт персон без даты рождения

--- gramps60/download/CompletenessGramplet/completeness_dashboard.py
+++ gramps60/download/CompletenessGramplet/completeness_dashboard.py
@@ -14,13 +14,33 @@ class CompletenessGramplet(Gramplet):
         self.connect(self.dbstate.db, "person-rebuild", self.update)
 
     def main(self):
+        self.set_text("Считаю...\n")
+        yield True
+
         db = self.dbstate.db
-        total = db.get_number_of_people()
+        handles = db.get_person_handles()
+        total = len(handles)
 
         if total == 0:
             self.set_text("База данных пуста\n")
             yield False
             return
 
-        self.set_text("Всего персон: %d\n" % total)
+        no_birth_date = 0
+        for handle in handles:
+            yield True
+            person = db.get_person_from_handle(handle)
+            birth_ref = person.get_birth_ref()
+            if not birth_ref:
+                no_birth_date += 1
+            else:
+                ev = db.get_event_from_handle(birth_ref.ref)
+                if ev.get_date_object().is_empty():
+                    no_birth_date += 1
+
+        self.set_text("")
+        self.append_text("Всего персон: %d\n\n" % total)
+        self.append_text("Без даты рождения: %d (%.0f%%)\n" % (
+            no_birth_date, 100 * no_birth_date / total))
+        self.append_text("", scroll_to="begin")
         yield False
