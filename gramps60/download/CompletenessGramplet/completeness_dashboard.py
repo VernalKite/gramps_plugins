8226bad6eeb75bd2f3c1beedd580a6f4b5711757
Родион Карпов <r.karpov3332mail.com@mail.ru>
2026-04-12 16:34:28 +0300

    добавление полей: дата смерти, место рождения, фото

--- gramps60/download/CompletenessGramplet/completeness_dashboard.py
+++ gramps60/download/CompletenessGramplet/completeness_dashboard.py
@@ -27,9 +27,14 @@ class CompletenessGramplet(Gramplet):
             return
 
         no_birth_date = 0
+        no_death_date = 0
+        no_birth_place = 0
+        no_photo = 0
+
         for handle in handles:
             yield True
             person = db.get_person_from_handle(handle)
+
             birth_ref = person.get_birth_ref()
             if not birth_ref:
                 no_birth_date += 1
@@ -37,10 +42,25 @@ class CompletenessGramplet(Gramplet):
                 ev = db.get_event_from_handle(birth_ref.ref)
                 if ev.get_date_object().is_empty():
                     no_birth_date += 1
+                if not ev.get_place_handle():
+                    no_birth_place += 1
+
+            death_ref = person.get_death_ref()
+            if not death_ref:
+                no_death_date += 1
+            else:
+                ev = db.get_event_from_handle(death_ref.ref)
+                if ev.get_date_object().is_empty():
+                    no_death_date += 1
+
+            if not person.get_media_list():
+                no_photo += 1
 
         self.set_text("")
         self.append_text("Всего персон: %d\n\n" % total)
-        self.append_text("Без даты рождения: %d (%.0f%%)\n" % (
-            no_birth_date, 100 * no_birth_date / total))
+        self.append_text("Без даты рождения:  %d (%.0f%%)\n" % (no_birth_date,  100 * no_birth_date  / total))
+        self.append_text("Без даты смерти:    %d (%.0f%%)\n" % (no_death_date,  100 * no_death_date  / total))
+        self.append_text("Без места рождения: %d (%.0f%%)\n" % (no_birth_place, 100 * no_birth_place / total))
+        self.append_text("Без фото:           %d (%.0f%%)\n" % (no_photo,       100 * no_photo       / total))
         self.append_text("", scroll_to="begin")
         yield False
